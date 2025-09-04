8f3903c5582a332799dad5746851eaaebd18055a
borninwinter56 <borninwinter56@gmail.com>
2025-09-04 20:50:44 +0200

    Added graceful shutdown and waiting for finishing of requests Makefile updated

--- api/init.go
+++ api/init.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -27,7 +27,14 @@ func (a *Api) initHttpServer() {
 	}
 }
 
+// Start beggins Listen and Serve parting it into a new goroutine.
+// It is also waiting for an interruption so it performs shutdown.
+// Shuts Down Gracefully; waits for the API to finish all requests that are being handled at the moment of shutdown.
 func (a *Api) Start(port string) {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// ListenAndServe is parted in a new goroutine so it does not block the handling of the shutdown below
 	go func() {
 		log.Println("Starting skeleton service...")
 		if err := a.HttpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -36,13 +43,14 @@ func (a *Api) Start(port string) {
 		}
 	}()
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
+	// Listen for the interrupt signal
+	<-ctx.Done()
 
-	// waits for shutdown signal
-	<-quit
+	stop()
 	log.Println("Shutting down skeleton service...")
 
+	// This context is used to notify service that it has 10 seconds to finish
+	// the requests that it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := a.HttpServer.Shutdown(ctx); err != nil {
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
+run: build
+	@./bin/skeleton
+
 build:
 	go build -o bin/skeleton ./cmd/skeleton/
 	@echo "Build completed!"
